53149f927a91084fd1f5f63c55eedfa23dd399b4
Mello <mellojerichomalig@gmail.com>
2022-07-12 13:18:10 +0800

    Deploy react app to github pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <h2>Portfolio of Mallow yay</h2>
+    </div>
+  );
+}
+
+export default App;
