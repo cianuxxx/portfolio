8d5d157f08919c8629e28fd51e5482e5f5809406
Mello <mellojerichomalig@gmail.com>
2022-07-26 09:32:41 +0800

    deploy

--- src/Contact.js
+++ src/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
                     <a className='contacts-h5' href ='https://drive.google.com/file/d/10PPNQtJhdDDn010BtSFUXESlNUuB4A8B/view?usp=sharing' target="_blank">Resume</a>
                 </div>
                 <div className="contacts">
-                    <h5 className='contacts-h5 end'>@2022 Mello Malig - 09087164663 - Contact</h5>
+                    <h5 className='contacts-h5 end'>@2022 Mello Malig - 09087164663 - mellojerichomalig@gmail.com</h5>
                 </div>
             </div>
         </body>
