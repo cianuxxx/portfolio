f958bd5aa51b10320f36f0834e6e42921bd24e03
Mello <mellojerichomalig@gmail.com>
2022-08-11 15:23:20 +0800

    Created title and logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Experience from './Experience';
 import Home from './Home';
 import Involvement from './Involvement';
 import Navbar from './Navbar'
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
+    
     <div className="App">
+      <div className="application">
+          <Helmet>
+              <meta charSet="utf-8" />
+              <title>Mello Portfolio</title>
+              <meta name="description" content="My Portfolio" />
+          </Helmet>
+      </div>
       <Navbar />
       <Home />
       <Experience />
